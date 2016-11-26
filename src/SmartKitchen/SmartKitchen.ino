e0f36031024150c2aedc51fb2101aa764317bc0f
Eduardo López <ed.lopez.morin@gmail.com>
2016-11-26 13:34:35 -0400

    Added classes for the switcher

--- src/SmartKitchen/SmartKitchen.ino
+++ src/SmartKitchen/SmartKitchen.ino
@@ -1,9 +1,12 @@
 
+
+#define MaxHeat 255
+
+
 int LowLedRing = 9;           // the PWM pin the LED is attached to
 int fadeAmount = 1;    // how many points to fade the LED by
 int RingHeatDelay = 30;
 int RingCoolDelay = 200;
-int MaxHeat =255;
 
 class HeatRing
 {
@@ -65,20 +68,81 @@ class HeatRing
     }
 };
 
-HeatRing LowHeatRing (9,fadeAmount,RingHeatDelay,RingCoolDelay,0);
-HeatRing MidHeatRing (10,fadeAmount,RingHeatDelay,RingCoolDelay,255);
-HeatRing HighHeatRing (11,fadeAmount,RingHeatDelay,RingCoolDelay,255);
+class Switch
+{ 
+  private:
+    const static int MaxPinsSize = 4;
+    int OffValue;
+    int Pins[MaxPinsSize]={0,0,0};
+    int PinsSize;
+    int MaxReads;
+    
+    int ReadValue()
+    {
+      int switchValues = 0;
+      for(int i =0 ; i < PinsSize ; ++i)
+      {
+        switchValues = switchValues << 1;
+        switchValues |= digitalRead(Pins[i]);
+      }
 
-void setup() {
+      if(switchValues == OffValue)
+        return 0;
+
+      int res = 1;
+      for(int i = 0 ; i < PinsSize ; ++i)
+      {
+        if((switchValues & 1) == 0)
+          return res;
+        switchValues = switchValues >> 1;
+        res++;
+      }
+      
+      return 0; 
+    }
+    
+  public:
+    
+    Switch(int pin[], int pinsSize, int maxReads)
+    {
+      PinsSize=pinsSize;
+      OffValue = (2 << (PinsSize-1))-1;
+      
+      for(int i =0 ; i < PinsSize ; i++){
+        Pins[i]=pin[i];
+        pinMode(Pins[i],INPUT_PULLUP);
+      }
+      MaxReads = maxReads;
+    }
+
+    int GetValue()
+    {
+      Serial.print(ReadValue());
+      Serial.print("\n");
+      return ReadValue();
+    }
+
   
- // declare pin 9 to be an output:
+};
+//HeatRing LowHeatRing (9,fadeAmount,RingHeatDelay,RingCoolDelay,0);
+//HeatRing MidHeatRing (10,fadeAmount,RingHeatDelay,RingCoolDelay,0);
+//HeatRing HighHeatRing (11,fadeAmount,RingHeatDelay,RingCoolDelay,0);
+
+
+Switch ss ((int[]){2,4,7},3,2);
+void setup() {
+
+  
+  pinMode(4,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
  Serial.begin(9600);
 }
 
 // Read the switch and get the value of the intensity from 0 to 3
 int PowerSwitchCheck()
 {
- return 1;
+  ss.GetValue();
+ return 3;
 }
 
 void BurnerActions()
@@ -91,7 +155,7 @@ void BurnerActions()
  PanCheck();
  ProximityCheck();
  
- LedPowerModule(powerValue);
+ //LedPowerModule(powerValue);
 }
 
 //Sound feedback depending on the power value
@@ -111,9 +175,9 @@ void LedPowerModule(int powerValue)
 {
   //Simulate the heating and cooling process of the leds
   // Control the intensity  or amount of heat rings on
-   LowHeatRing.Heat( powerValue > 0);
-   MidHeatRing.Heat( powerValue > 1);
-   HighHeatRing.Heat( powerValue > 2);
+ //  LowHeatRing.Heat( powerValue > 0);
+  // MidHeatRing.Heat( powerValue > 1);
+   //HighHeatRing.Heat( powerValue > 2);
    
   HeatSimulator(powerValue);
   IntensitySimulator(powerValue);
@@ -133,10 +197,10 @@ void IntensitySimulator(int powerValue)
 // Check if someone is close to the burner and give feedbacks
 void ProximityCheck()
 {
-  if(LowHeatRing.IsHot())
-  {
+//  if(LowHeatRing.IsHot())
+  //{
     // do something
-  }
+  //}
 }
 
 // Check is something is boiling and gives feedbacks
